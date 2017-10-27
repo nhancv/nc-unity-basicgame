82d0249e30d49b1bee6876c0f318fc954d64585a
Nhan Cao <nhancv92@gmail.com>
2017-10-27 15:17:50 +0700

    update source

---
+++ Assets/Scripts/Patrol.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour {
+
+	public Transform[] patrolPoints;
+	public float moveSpeed = 5;
+	private int currentPoint;
+
+	// Use this for initialization
+	void Start () {
+		currentPoint = 0;
+		transform.position = patrolPoints [0].position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position == patrolPoints [currentPoint].position) {
+			currentPoint++;
+		}
+
+		if (currentPoint >= patrolPoints.Length) {
+			currentPoint = 0;
+		}
+		transform.position = Vector3.MoveTowards (transform.position, patrolPoints [currentPoint].position, moveSpeed * Time.deltaTime);
+	}
+
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float moveSpeed = 50;
+	public GameObject deathParticles;
+
+	private float maxSpeed = 5f;
+	private Vector3 input;
+	private Rigidbody rigidBody;
+
+	private Vector3 spawn;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody>();
+		spawn = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		input = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical"));
+		if (rigidBody.velocity.magnitude < maxSpeed) {
+			rigidBody.AddForce (input * moveSpeed);
+		}
+	}
+
+	void OnCollisionEnter(Collision other) {
+		if (other.transform.tag == "Enemy") {
+			Instantiate (deathParticles, transform.position, Quaternion.identity);
+			transform.position = spawn;
+		}
+
+	}
+
+}
